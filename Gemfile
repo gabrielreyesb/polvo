b3b4a2056475ee01339648cb726ac8c00c3a4837
Gabriel <gabriel@handy.la>
2024-06-11 11:57:41 -0600

    Agregado caso de prueba wehhook rapid7

--- Gemfile
+++ Gemfile
@@ -13,6 +13,10 @@ gem "puma", ">= 5.0"
 
 gem "faraday"
 
+group :test do
+  gem 'webmock'
+end
+
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 # gem "jbuilder"
 
--- test/controllers/webhooks_controller_test.rb
+++ test/controllers/webhooks_controller_test.rb
@@ -1,7 +1,36 @@
-require "test_helper"
+require 'test_helper'
+require 'webmock/minitest' 
 
 class WebhooksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should receive Rapid7 webhook and post to Basecamp" do
+    webhook_payload = {
+      'alert' => {
+        'id' => 'ALERT_123',
+        'name' => 'Test Alert',
+        'status' => 'new',
+      },
+      'metadata' => {
+        'severity' => 'high',
+        'attackType' => 'phishing',
+      }
+    }
+
+    stub_request(:post, "https://3.basecamp.com/3734616/integrations/5jLtTZFkYWebruRpZrpejfEZ/buckets/7028920/chats/975348750/lines")
+      .with(
+        body: hash_including({
+          subject: "Form Submission",
+          content: "<details><summary>🚨 Alerta desde Rapid7: <b>Test Alert</b></summary><pre>#{JSON.pretty_generate(webhook_payload)}</pre><br><i>Lanzado desde la app de integraciones internas.</i></details>"
+        }),
+        headers: {
+          'Content-Type' => 'application/json'
+        }
+      )
+      .to_return(status: 201, body: "", headers: {})
+
+    post webhooks_rapid7_url, params: webhook_payload.to_json, headers: { 'Content-Type' => 'application/json' }
+
+    assert_response :ok
+    assert_requested :post, "https://3.basecamp.com/3734616/integrations/5jLtTZFkYWebruRpZrpejfEZ/buckets/7028920/chats/975348750/lines", times: 1  # Ensure the Basecamp request was made
+  end
 end
+
